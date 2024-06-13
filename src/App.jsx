446a8a4c7788ae5b923656fba683217a9c3f7cfd
[codieshiv] <[codieshiv.web@gmail.com]>
2024-06-13 09:13:51 +0530

    react+tailwind+3d

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+import 'atropos/css'
+
+
+//   return (
+//     <>
+// export default function App() {
+  
+//     <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+
+// }
+//     </>
+//   )
+
+export default function App() {
+  return (
+    <>
+      <div className="atropos my-atropos">
+  
+  <div className="atropos-scale">
+
+    <div className="atropos-rotate">
+      
+      <div className="atropos-inner">
+  
+      </div>
+    </div>
+  </div>
+</div>
+    </>
+  );
+}
+
+
+
+
+// export default function App() {
